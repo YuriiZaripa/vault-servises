5a72df63cf15c61c50d451fee6179d0f8785d46d
YuraZaripa <161350035+YuraZaripa@users.noreply.github.com>
2024-06-27 13:18:34 +0300

    init

---
+++ src/main/java/org/example/testvaultservice/TestVaultServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.testvaultservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVaultServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestVaultServiceApplication.class, args);
+    }
+
+}
